a51c6dcd93b6d33dc3e0b8f8809a119179bf1897
eugenyongesa@gmail.com <eugenyongesa@gmail.com>
2025-10-15 09:18:29 +0300

    Implement Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,91 @@
+import Link from "next/link";
+import React from "react";
+import Logo from "./Logo";
+import { useRouter } from "next/router";
+import { Facebook, GitHub, Instagram, LinkedIn, TwitterIcon } from "./Icon";
+import { motion } from "framer-motion";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+
+      <span
+        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5
+       group-hover:w-full transition-[width] ease duration-300 ${
+         router.asPath === href ? "w-full" : "w-0"
+       }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+function NavBar() {
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+      <nav>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mr-4" />
+        <CustomLink href="/projects" title="Projects" className="mr-4" />
+        <CustomLink href="/articles" title="Articles" className="mr-4" />
+      </nav>
+
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="http://twitter.com"
+          target="_blank"
+          className="w-6 mx-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <GitHub />
+        </motion.a>
+        <motion.a
+          href="http://facebook.com"
+          target="_blank"
+          className="w-6 mx-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <LinkedIn />
+        </motion.a>
+        <motion.a
+          href="http://instagram.com"
+          target="_blank"
+          className="w-6 mx-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <Facebook />
+        </motion.a>
+        <motion.a
+          href="http://github.com"
+          target="_blank"
+          className="w-6 mx-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <Instagram />
+        </motion.a>
+        <motion.a
+          href="http://linkedin.com"
+          target="_blank"
+          className="w-6 ml-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <TwitterIcon />
+        </motion.a>
+      </nav>
+
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+}
+
+export default NavBar;
